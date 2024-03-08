3dad487672091d7fcc8140dd4eecc00033e9efbe
KinRyuSama <nathanv.collectionneur@gmail.com>
2024-03-08 12:35:45 -0500

    Maya_0.0.4

---
+++ src/tools/read_files.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import Type
+
+from src.models.state import State
+from src.models.tool import Tool
+
+
+class _Args(BaseModel):
+    text: str
+    file_path: str
+
+
+class ReadFiles(Tool):
+    name: str = "read_files"
+    description: str = "Used to test that tools work correctly."
+    args_type: Type[BaseModel] = _Args
+
+    def _run(self, state: State, args: _Args) -> str:
+        try:
+            with open(args.file_path, "r") as file:
+                file_contents = file.read()
+            return file_contents
+        except Exception as e:
+            print(f"Error reading file {args.file_path}: {e}")
+            return None
