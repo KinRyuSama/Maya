3e6247317d623e10e3f13e9c6b77b3cd66ecd036
KinRyuSama <nathanv.collectionneur@gmail.com>
2024-05-31 18:47:56 -0400

    Create/Delete files Revamp 2Tools

--- src/data/tools.py
+++ src/data/tools.py
@@ -2,22 +2,24 @@ from functools import cache
 
 from src.models.tool import Tool
 from src.tools.echo import Echo
-from src.tools.create_delete import Create_Delete
+from tools.deletefile import Delete_File
 from src.tools.list_files import ListFiles
 from src.tools.manage_folders import ManageFolders
 from src.tools.read_file import ReadFile
 from src.tools.update_file import UpdateFile
 from src.tools.parser import Parser
+from src.tools.makefile import Create_File
 
 
 @cache
 def list_tools() -> list[Tool]:
     return [
         Echo(),
-        Create_Delete(),
         ListFiles(),
         ManageFolders(),
         ReadFile(),
         UpdateFile(),
         Parser(),
+        Create_File(),
+        Delete_File(),
     ]
