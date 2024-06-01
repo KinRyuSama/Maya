acf05760777a0c458bcfa536494277e37b10f1ce
KinRyuSama <nathanv.collectionneur@gmail.com>
2024-05-31 20:45:12 -0400

    Maya 1.2 (please work)

--- src/tools/deletefile.py
+++ src/tools/deletefile.py
@@ -9,67 +9,19 @@ import os
 
 class _Args(BaseModel):
     file_name: str
-<<<<<<< HEAD
-<<<<<<< HEAD
     action: Literal["delete"]
 
 
 class DeleteFile(Tool):
     name: str = "Delete_File"
     description: str = "delete files in folder"
-=======
-=======
->>>>>>> 3e6247317d623e10e3f13e9c6b77b3cd66ecd036
-    action: Literal["create"]
-
-
-<<<<<<<< HEAD:src/tools/createfile.py
-class CreateFile(Tool):
-    name: str = "create_file"
-    description: str = "create files in folder"
-    args_type: Type[BaseModel] = _Args
-
-    def _run(self, state: State, args: _Args) -> str:
-        if args.action == "create":
-            self._create_file(args.file_name)
-<<<<<<< HEAD
-=======
-
-class Delete_File(Tool):
-    name: str = "Delete File"
-    description: str = "Create and Delete files in folder"
-    args_type: Type[BaseModel] = _Args
-
-    def _run(self, state: State, args: _Args) -> str:
->>>>>>> 3e6247317d623e10e3f13e9c6b77b3cd66ecd036
-========
-class Delete_File(Tool):
-    name: str = "Delete File"
-    description: str = "Create and Delete files in folder"
-<<<<<<< HEAD
->>>>>>> 3e6247317d623e10e3f13e9c6b77b3cd66ecd036
     args_type: Type[BaseModel] = _Args
 
     def _run(self, state: State, args: _Args) -> str:
         if args.action == "delete":
             self._delete_file(args.file_name)
-<<<<<<< HEAD
         else:
             raise ValueError("Invalid action specified. Must be 'create' or 'delete'.")
-        return f"Successfully {args.action}ed file {args.file_name}"
-
-=======
->>>>>>>> 3e6247317d623e10e3f13e9c6b77b3cd66ecd036:src/tools/deletefile.py
-=======
-    args_type: Type[BaseModel] = _Args
-
-    def _run(self, state: State, args: _Args) -> str:
->>>>>>>> 3e6247317d623e10e3f13e9c6b77b3cd66ecd036:src/tools/deletefile.py
-        if args.action == "delete":
-            self._delete_file(args.file_name)
->>>>>>> 3e6247317d623e10e3f13e9c6b77b3cd66ecd036
-        else:
-            raise ValueError("Invalid action specified. Must be 'create'.")
         return f"Successfully {args.action}ed file {args.file_name}"
 
     def _delete_file(self, file_name: str):
